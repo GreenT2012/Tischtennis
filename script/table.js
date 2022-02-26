8de6c09453d473f3d2b5ce06e985ccba3b17ceaa
Bob <t.klattenhoff@gmx.de>
2022-02-27 00:26:35 +0100

    Tabelle hinzugefügt

---
+++ script/table.js
@@ -0,0 +1,11 @@
+/*let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+// Adding the entire table to the body tag
+document.getElementById('fulltable').appendChild(table);*/
+let tableheaders = ["#", "Name", "Punte","Gespielte Spiele"]
+
